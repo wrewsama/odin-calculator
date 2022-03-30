2fee8fad21b82e4ff9682271fa6a6e8eb6ede9de
wrewsama <andrewlzs512@gmail.com>
2022-03-30 21:19:16 +0800

    Set up UI.

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(x ,y) {
+    return x + y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(op, first, second) {
+    return op(first, second);
+}
